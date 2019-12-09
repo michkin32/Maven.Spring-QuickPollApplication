203a9b40b56a3923fbd8e5209df5e5778ce48c87
Mike King <michkin32@gmail.com>
2019-12-09 09:02:59 -0500

    Working on exceptions

---
+++ src/main/java/dtos/error/ErrorDetail.java
@@ -0,0 +1,57 @@
+package dtos.error;
+
+public class ErrorDetail {
+    String title;
+    int status;
+    String detail;
+    long timeStamp;
+    String developerMessage;
+
+    public ErrorDetail(String title, int status, String detail, long timeStamp, String developerMessage) {
+        this.title = title;
+        this.status = status;
+        this.detail = detail;
+        this.timeStamp = timeStamp;
+        this.developerMessage = developerMessage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getDeveloperMessage() {
+        return developerMessage;
+    }
+
+    public void setDeveloperMessage(String developerMessage) {
+        this.developerMessage = developerMessage;
+    }
+}
---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/controllers/RestExceptionHandler.java
@@ -0,0 +1,17 @@
+package io.zipcoder.tc_spring_poll_application.controllers;
+
+import org.springframework.boot.context.config.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe, HttpServletRequest request) {
+        return null;
+    }
+}
